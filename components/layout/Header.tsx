9e36137d28b4302ea67055a59b26b0e6924c3453
Pascal Emmenegger <pemmenegger@baug-ibb-hix-client-1-a-104.intern.ethz.ch>
2025-04-08 11:47:48 +0200

    header implemented

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,26 +1,52 @@
-// components/Header.tsx
+import { useRouter } from "next/router";
+
 export default function Header() {
+  const router = useRouter();
+
+  const handleHomeClick = () => {
+    router.push("/");
+  };
+
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
-    <header className="flex items-center justify-between  p-2">
-      {/* Left Controls */}
-      <div className="flex space-x-2">
-        <button className="w-8 h-8 flex items-center justify-center bg-gray-200 rounded-full text-gray-500">
-          ⬅️
-        </button>
-        <button className="w-8 h-8 flex items-center justify-center bg-gray-200 rounded-full text-gray-500">
-          ➡️
-        </button>
+    <header className="flex items-start justify-between">
+      <div className="flex flex-col space-y-[6px]">
+        <div
+          className="w-[49px] h-[33px] flex items-center justify-center rounded-r-full bg-gray-300"
+          onClick={handleHomeClick}
+        >
+          <img
+            src={`/icons/home.svg`}
+            alt="Home"
+            className="w-[21px] h-[21px] cursor-pointer"
+          />
+        </div>
+
+        <div
+          className="w-[49px] h-[33px] flex items-center justify-center rounded-r-full bg-gray-300"
+          onClick={handleHomeClick}
+        >
+          <img
+            src={`/icons/back.svg`}
+            alt="Back"
+            className="w-[22px] h-[23px] cursor-pointer"
+            onClick={handleBackClick}
+          />
+        </div>
       </div>
 
-      {/* Center Button */}
-      <button className="bg-gray-300 text-white px-4 py-1 rounded text-sm font-semibold">
-        INSIEME
-      </button>
+      <div className="h-[33px] w-[182px] bg-gray-300 flex items-center justify-center rounded-b-xl">
+        <p className="text-white rounded-b-xl text-xl font-semibold">INSIEME</p>
+      </div>
 
-      {/* Right Button */}
-      <button className="bg-gray-300 text-white px-4 py-1 rounded text-sm font-semibold">
-        FULL MOVIE
-      </button>
+      <div className="h-[56px] w-[182px] bg-gray-300 flex items-center justify-center rounded-bl-xl">
+        <p className="text-white rounded-b-xl text-xl font-semibold">
+          FULL MOVIE
+        </p>
+      </div>
     </header>
   );
 }
